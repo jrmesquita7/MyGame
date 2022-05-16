f60f426522bea9b89f068e5093be7d6db2ad5440
jrmesquita7 <cleobetojunior1999@gmail.com>
2022-05-16 16:42:27 -0300

    first

--- main.lua
+++ main.lua
@@ -1,221 +1,231 @@
+-- variáveis de altura e lagura da tela
+larguraTela = love.graphics.getWidth() 
+alturaTela = love.graphics.getHeight()
+-- variáveis de altura e lagura da tela
+
+--Informações do Jogador
+local player = {          
+ imagem,
+pos_pl_x = larguraTela / 2,
+pos_pl_y = alturaTela / 2,
+veloc = 200
+}
+--Informações do Jogador
+
 function love.load()
-    love.window.setMode(1000, 768)
-
-
-    anim8 = require 'libraries/anim8/anim8'
-    sti = require 'libraries/Simple-Tiled-Implementation/sti'
-    cameraFile = require 'libraries/hump/camera'
-
-    cam = cameraFile()
-    
-    myFont = love.graphics.newFont("fonts/Herculanum.ttf", 30)
-    --myfont = love.graphics.newFont("fonts/myfont.ttf", 30)
-
-    sounds = {}
-    sounds.jump = love.audio.newSource("audio/jump1.wav", "static")
-    sounds.music = love.audio.newSource("audio/music2.mp3", "stream")
-    sounds.music:setLooping(true)
-    sounds.music:setVolume(0.3)
-    sounds.music:play()
-
-    sprites = {}
-    sprites.playerSheet = love.graphics.newImage('sprites/spritesheet1.png')
-    sprites.enemySheet = love.graphics.newImage('sprites/zombie.png')
-    sprites.background = love.graphics.newImage('sprites/Background.png')
-    sprites.coinSheet = love.graphics.newImage('maps/coin.png')
-
-
-    local grid = anim8.newGrid(416, 454, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
-    local enemyGrid = anim8.newGrid(102, 90, sprites.enemySheet:getWidth(), sprites.enemySheet:getHeight())
-    --local collectibleGrid = anim8.newGrid(32, 32, sprites.collectibleSheet:getWidth(), sprites.enemySheet:getHeight())
-
-    animations = {}
-    animations.idle = anim8.newAnimation(grid('1-7', 1), 0.08)
-    animations.jump = anim8.newAnimation(grid('8-17', 1), 0.08)
-    animations.run = anim8.newAnimation(grid('18-27', 1), 0.08)
-    animations.enemy = anim8.newAnimation(enemyGrid('4-10', 1), 0.9)
-    --animations.collectible = anim8.newAnimation(collectibleGrid('1-2', 1), 1)
-
-    wf = require 'libraries/windfield/windfield'
-    world = wf.newWorld(0, 800, false)
-    world:setQueryDebugDrawing(true)
-
-    world:addCollisionClass('Platform')
-    world:addCollisionClass('Player' --[[{ignores = {'Platform'}}]])
-    world:addCollisionClass('Danger')
-    world:addCollisionClass('Collect', {ignores = {'Player'}})
-    
-
-    require('player')
-
-    require('enemy')
-    require('coins')
-
-    require('libraries/show')
-
-    dangerZone = world:newRectangleCollider(-500, 800, 6000, 50, {collision_class = "Danger"})
-    dangerZone:setType('static')
-
-    platforms = {}
-    
-
-    gemX = 0
-    gemY = 0
-
-
-    score = 0
-
-    saveData = {}
-    saveData.currentLevel = "level1"
-
-    if love.filesystem.getInfo("data.lua") then
-        local data = love.filesystem.load("data.lua")
-        data()
-    end
-
-    loadMap(saveData.currentLevel)
-    --spawnCoins()
-
+  --Informações dos disparos
+  atira = true
+  dlDisp = 0.5
+  tmpAtirar = dlDisp
+  tiros = {}
+  imagemTiro = love.graphics.newImage("luva.png")
+  --Informações dos disparos
+  
+  --Informações do Inimigo
+  dlInimigo = 0.3
+  tmpInimigo = dlInimigo
+  imagemInimigo = love.graphics.newImage("gato.png") 
+  inimigos = {}
+  --Informações do Inimigo
+  
+  --Informações sobre vida e pontos
+  contVivo = true
+  pontos = 0
+  --Informações sobre vida e pontos
+  
+  --Informações sobre o cenário
+  cenario = love.graphics.newImage("cenario.png")
+  cenarioCont = love.graphics.newImage("cenario.png") --cria o background infinito
+  fundo = {
+  x = 0,                    -- posição do primeiro background
+  y = 0,                    -- posição do primeiro background
+  y2 = 0 - cenario:getHeight(),  -- posição do segundo background
+  vel = 30
+  }
+  --Informações sobre o cenário
+  
+  --Informações sobre pontuação
+  fonte = love.graphics.newImageFont("fonte.png", " abcdefghijklmnopqrstuvwxyz".."ABCDEFGHIJKLMNOPQRSTUVWXYZ0" .."123456789.,!?-+/():;%&`´*#=[]¨")
+  --Informações sobre pontuação
+  
+  --Informações sobre o audio jogo
+  audioDisp = love.audio.newSource("audio/disparoluva.mp3", "static")               -- arq de audio do disparos
+  explosaoPlay = love.audio.newSource("audio/playexplosao.wav", "static")       -- arq de audio da explosao da nave
+  explosaoInim = love.audio.newSource("audio/miauexplosao.mp3", "static")    -- arq de audio da explosão do inimigo 
+  musicaFundo = love.audio.newSource("audio/fundo.mp3", "stream")         -- arq de audio da musica de fundo
+  musicaFundo: play()   --função para tocar a musica de fundo desde o começo do jogo
+  musicaFundo: setLooping(true)  --função que faz a musica de fundo recomeçar ao termino
+  --Informações sobre o audio jogo
+  
+  --atribui uma variável a imagem do jogador
+ player.imagem = love.graphics.newImage("test.png")
+ --atribui uma variável a imagem do jogador
 end
 
 function love.update(dt)
+  movimentacao(dt) --atualiza os movimentos do jogador
+  atirar(dt) -- atualiza a função atirar
+  inimigo(dt) --atualiza a função inimigo
+  colisao()   --atualiza a função colisão
+  movfundo(dt)  --dá o sentido de continuidade no cenário
+  reincia() -- função para reiniciar o jogo
+  
+end
 
-    world:update(dt)
-    gameMap:update(dt)
-    playerUpdate(dt)
-    updateEnemies(dt)
-    updateCoins()
-    local px, py = player:getPosition()
-
-    cam:lookAt(px, love.graphics.getHeight()/2)
-
-    local colliders = world:queryCircleArea(gemX, gemY, 10, {"Player"})
-    if #colliders > 0 then
-        if saveData.currentLevel == "level1" then
-            loadMap("level2")
-        elseif saveData.currentLevel == "level2" then
-            loadMap("level1")
-        end
+function movimentacao(dt)
+  --Movimentação do jogador
+  if love.keyboard.isDown("right") then
+      if player.pos_pl_x < (larguraTela - player.imagem:getWidth() / 2 ) then --Laço para delimitar a movimentação do jogador dentro da tela
+      player.pos_pl_x = player.pos_pl_x + player.veloc * dt 
     end
+  end
+  if love.keyboard.isDown("left") then
+    if player.pos_pl_x > (0 + player.imagem:getWidth() / 2 ) then --Laço para delimitar a movimentação do jogador dentro da tela
+      player.pos_pl_x = player.pos_pl_x - player.veloc * dt 
+    end
+  end
+  if love.keyboard.isDown("up") then
+    if player.pos_pl_y > (0 + player.imagem:getHeight() / 2 ) then --Laço para delimitar a movimentação do jogador dentro da tela
+      player.pos_pl_y = player.pos_pl_y - player.veloc * dt
+    end
+  end
+  if love.keyboard.isDown("down") then
+    if player.pos_pl_y < (alturaTela - player.imagem:getHeight() / 2 ) then --Laço para delimitar a movimentação do jogador dentro da tela
+      player.pos_pl_y = player.pos_pl_y + player.veloc * dt
+    end
+  end
+ end 
+  --Movimentação do jogador
+  
+function atirar(dt)
+  --Função para disparar
+  tmpAtirar = tmpAtirar - (2 * dt)
+  if tmpAtirar < 0 then
+      atira = true
+  end
+  if contVivo then
+    if love.keyboard.isDown("space") and atira then
+      proxDisp = {x = player.pos_pl_x, y = player.pos_pl_y, img = imagemTiro}
+      table.insert(tiros, proxDisp)
+      audioDisp:stop()    --responsável por executar o som dos disparos assim que se deixa de apertado o botão de atirar
+      audioDisp:play()   --responsável por executar o som dos disparos assim que apertado o botão de atirar
+      atira = false
+      tmpAtirar = dlDisp
+    end
+  end  
+  for i, tiro in ipairs(tiros) do
+      tiro.y = tiro.y - (500 * dt)
+      if tiro.y < 0 then
+        table.remove(tiros, i)
+      end
+    end
+  end
+  --Função para disparar
+  
+function inimigo(dt)
+  --Função Inimigos e suas disposições na tela
+   tmpInimigo = tmpInimigo - (1 * dt)
+   if pontos > 5 then
+    tmpInimigo = tmpInimigo - (5 * dt)
+    tmpAtirar = tmpAtirar - (5 * dt)
 
+  end
+   if tmpInimigo < 0 then
+     tmpInimigo = dlInimigo
+     inimAleatorio = math.random(10, love.graphics.getWidth() - ((imagemInimigo:getWidth() / 2) + 10))
+   proxInimigo = {x = inimAleatorio, y = -imagemInimigo:getWidth(), img = imagemInimigo}
+   table.insert(inimigos, proxInimigo)
+  end
+  for i, inimigo in ipairs (inimigos) do  --verifica os inimigos na tabela inimigos e remove
+    inimigo.y = inimigo.y + (200 * dt)
+    if inimigo.y > 850 then
+        table.remove (inimigos, i) -- remove os inimigos da tabela
+      end
+    end
+  end
+--Função Inimigos e suas disposições na tela
+
+function colisao()
+  for i, inimigo in ipairs(inimigos) do --verifica os inimigos existentes
+    for j, tiro in ipairs(tiros) do --verifica os disparos existentes
+      if verifColisao (inimigo.x, inimigo.y, imagemInimigo:getWidth(), imagemInimigo:getHeight(),tiro.x, tiro.y,imagemInimigo:getWidth(), imagemInimigo:getHeight()) then -- condição de colisão tirando os disparos e o inimigo da tela
+        table.remove (tiros, j)
+        table.remove (inimigos, i)
+        explosaoInim : stop()  -- deixa o audio parado até que seja executado
+        explosaoInim : play()  --executa assim que o inimigo é retirado da tabela
+        pontos = pontos + 1
+      end
+    end
+    if verifColisao(inimigo.x, inimigo.y, imagemInimigo:getWidth(), imagemInimigo:getHeight(), player.pos_pl_x - (player.imagem:getWidth() / 2), player.pos_pl_y, player.imagem:getWidth(), player.imagem:getHeight()) and contVivo then
+      table.remove(inimigos, i) --verifica a colisão do inimigo com a nave
+      explosaoPlay:play()     --executa quando a nave é destruida, se uso o stop pois não é executado repetidamente.
+      contVivo = false
+    end
+  end
+end
+
+function verifColisao(pos_col_x1, pos_col_y1, pos_col_w1, pos_col_h1, pos_col_x2, pos_col_y2, pos_col_w2, pos_col_h2) --função para verificar as colisões
+  return pos_col_x1 < pos_col_x2 + pos_col_w2 and pos_col_x2 < pos_col_x1 + pos_col_w1 and pos_col_y1 < pos_col_y2 + pos_col_h2 and pos_col_y2 < pos_col_y1 + pos_col_h1
+end
+--função para verificar as colisões
+
+function movfundo(dt) --movimentação do plano de fundo
+  fundo.y = fundo.y + fundo.vel * dt 
+  fundo.y2 = fundo.y2 + fundo.vel * dt
+ 
+ if fundo.y >alturaTela then
+  fundo.y = fundo.y2 - cenarioCont:getHeight()
+ end
+ if fundo.y2 > alturaTela then
+   fundo.y2 = fundo.y - cenario:getHeight()
+ end
+end
+
+function reincia()
+  --reiniciar o jogo, zera as tabelas, posiciona a nave novamente na posição inicial do jogo, zera os pontos
+  if not contVivo and love.keyboard.isDown('return') then
+    tiros = {}
+    inimigos = {}
+    
+    atira = tmpAtirar
+    tmpInimigo = dlInimigo
+    
+    player.pos_pl_x = larguraTela / 2
+    player.pos_pl_y = alturaTela / 2
+    
+    pontos = 0
+    contVivo = true
+  --reiniciar o jogo
+  end
 end
 
 function love.draw()
-
-    love.graphics.draw(sprites.background, 0, 0, nil, 0.5, 0.5)
-    love.graphics.setFont(myFont) 
-    love.graphics.printf("CHEESES:  ".. coins.count, 0, 0, love.graphics.getWidth() / 2 - 100, "left")
-
-    cam:attach()
-        gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
-        --world:draw()
-        drawPlayer()
-        drawEnemies()
-        drawCoins()
-    
-        if saveData.currentLevel == "level1" then
-            love.graphics.setFont(myFont) 
-            love.graphics.printf("Oi! Eu sou Garima. Bem-vindo ao meu Projeto Final!", 0, 0, love.graphics.getWidth() / 2 - 100, "left")
-            love.graphics.printf("Nós apenas temos que navegar pelos níveis e alcançar a Jóia no final.", 800, 0, love.graphics.getWidth() / 2 - 100, "left")
-            --love.graphics.printf(coins.count, 0, 0, love.graphics.getWidth() / 2 - 100, "left")
-            love.graphics.printf("Cuidado com os fantasmas!", 1600, 0, love.graphics.getWidth() / 2 - 100, "left")
-            love.graphics.printf("Veja esta jóia no final? Vamos agarrá-lo!", 2600, 0, love.graphics.getWidth() / 2 - 100, "left")
-        end
-        if saveData.currentLevel == "level2" then
-            love.graphics.setFont(myFont) 
-            love.graphics.printf("ESTE É O NÍVEL 2!", 0, 40, love.graphics.getWidth() / 2 - 100, "left")
-            love.graphics.printf("VAMOS PEGAR A ÚLTIMA JÓIA!!", 800, 0, love.graphics.getWidth() / 2 - 100, "left")
-            love.graphics.printf("OK! ENTÃO É ISSO. OBRIGADO PELA VISITA!", 3200, 40, love.graphics.getWidth() / 2 - 100, "left")
-            love.graphics.printf("OBRIGADO CS50!", 2800, 40, love.graphics.getWidth() / 2 - 100, "left")
-        end
-        --local colliders = world:queryCircleArea(collectiblesX, collectiblesY, 15, {"Player"})
-        --if #colliders > 0 then
-           -- if 
-        --end
-    cam:detach()
-
+  --Imprime o cenario
+  love.graphics.draw(cenario, fundo.x, fundo.y)
+  love.graphics.draw(cenarioCont, fundo.x, fundo.y2) 
+  --Imprime o cenario
+  
+  --pontuação
+  love.graphics.setFont(fonte) --carrega a imagem da fonte
+  love.graphics.print("Pontuacao: ".. pontos, 10, 10) --o pontuação e suas posições na tela
+  --pontuação
+  --imprime jogador, fim de jogo e reinicio
+  if contVivo then
+    love.graphics.draw(player.imagem, player.pos_pl_x, player.pos_pl_y, 0, 1, 1, player.imagem:getWidth() / 2, player.imagem:getHeight() / 2)
+  else
+    love.graphics.print ("Pressione 'ENTER' para reinicar.", larguraTela / 3, alturaTela / 2)
+  end
+  --imprimi jogador, fim de jogo e reinicio
+  
+  --imprime os disparos
+  for i, tiro in ipairs(tiros) do
+  love.graphics.draw(tiro.img, tiro.x, tiro.y, 0, 1, 1, imagemTiro:getWidth() / 2, imagemTiro:getHeight())
+  --imprime os disparos
 end
 
-function love.keypressed(key)
-    if key == 'space' then
-        if player.grounded then
-            player:applyLinearImpulse(0, -4800)
-            sounds.jump:play()
-        end
-    end
-    if key == 'r' then
-        loadMap("level2")
-    end
-
-end
-
-function love.mousepressed(x, y, button)
-    if button == 1 then
-        local colliders = world:queryCircleArea(x, y, 200, {'Platform', 'Danger'})
-        for i,c in ipairs(colliders) do
-            c:destroy()
-        end
-    end
-end
-
-
-function spawnPlatform(x, y, width, height)
-    local platform = world:newRectangleCollider(x, y, width, height, {collision_class = "Platform"})
-    platform:setType('static')
-    table.insert(platforms, platform)
-end
-
-
-function destroyAll()
-    local i = #platforms
-    while i > -1 do
-        if platforms[i] ~= nil then
-            platforms[i]:destroy()
-        end
-        table.remove(platforms, i)
-        i = i - 1
-    end
-
-    local i = #enemies
-    while i > -1 do
-        if enemies[i] ~= nil then
-            enemies[i]:destroy()
-        end
-        table.remove(enemies, i)
-        i = i - 1
-    end
-end
-
-function loadMap(mapName)
-    saveData.currentLevel = mapName
-    love.filesystem.write("data.lua", table.show(saveData, "saveData"))
-    
-    
-    destroyAll()
-    gameMap = sti("maps/" .. mapName .. ".lua")
-    
-    for i , obj in pairs(gameMap.layers["Start"].objects) do
-        playerStartX = obj.x
-        playerStartY = obj.y
-    end
-    player:setPosition(playerStartX, playerStartY)
-
-    for i , obj in pairs(gameMap.layers["Platforms"].objects) do
-        spawnPlatform(obj.x, obj.y, obj.width, obj.height)
-
-    end
-
-    for i , obj in pairs(gameMap.layers["Enemies"].objects) do
-        spawnEnemy(obj.x, obj.y)
-    end
-
-    for i , obj in pairs(gameMap.layers["Gem"].objects) do
-        gemX = obj.x
-        gemY = obj.y
-    end
-
-    for i , obj in pairs(gameMap.layers["Coins"].objects) do
-        spawnCoin(obj.x, obj.y)
-    end
-
+--imprime os inimigos
+for i, inimigo in ipairs(inimigos) do
+  love.graphics.draw(inimigo.img, inimigo.x, inimigo.y)
+  end
+--imprime os inimigos
 end
